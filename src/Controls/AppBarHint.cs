3dff2d556ecc82566e8a273379d28ad75c15e27e
Jeremy Alles <jeremy.alles@gmail.com>
2013-10-07 12:52:48 +0200

    Open app bar when hint control is tapped

--- src/Controls/AppBarHint.cs
+++ src/Controls/AppBarHint.cs
@@ -1,8 +1,27 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace Japf.Win8.AppBarHint.Demo.Controls
 {
     public class AppBarHint : Button
     {
+        protected override void OnTapped(TappedRoutedEventArgs e)
+        {
+            base.OnTapped(e);
+
+            if (Window.Current.Content is Frame)
+            {
+                var frame = (Frame) Window.Current.Content;
+                if (frame.Content is Page)
+                {
+                    var page = (Page) frame.Content;
+                    if (page.BottomAppBar != null)
+                    {
+                        page.BottomAppBar.IsOpen = true;
+                    }
+                }
+            }
+        }
     }
 }
